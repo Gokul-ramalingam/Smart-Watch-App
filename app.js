a71073d62f5ab21917fb3f04012b404f2aa271e6
Gokul <gokulramalingam1@gmail.com>
2019-09-05 20:57:52 +0530

    Implementation of message layout

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 function trial(){
-    document.getElementById("initialScreen").style.visibility = 'hidden';
+    document.getElementById("initialScreen").style.display = 'none';
     document.getElementsByClassName("button1")[0].style.backgroundColor = '#1890f0';
+    document.getElementById("messageScreen").style.display='block';
 }
+
+$( document ).ready(function() {
+    document.getElementById("messageScreen").style.display = 'none';
+});
+
+$(".button1").click(function(){
+    $("#receivedMessages").append("<h6>55555 <br><span>Sample text</span></h6>");
+  });
